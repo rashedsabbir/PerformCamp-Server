be4f040b97fdcaef103dedc32bff0d39aa532afb
rashedsabbir <rashedulhasan1996@gmail.com>
2022-08-12 20:01:37 +0600

    some changes

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId=require("mongodb").ObjectId
+require
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 //middleware
@@ -14,23 +19,31 @@ app.use(express.json())
 //live link = https://intense-citadel-07221.herokuapp.com
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0e6jqyu.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
 
 
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'UnAuthorized Access' });
-    }
-
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden Access' });
-        }
-        req.decoded = decoded;
-        next();
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({
+      message: 'UnAuthorized Access'
     });
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({
+        message: 'Forbidden Access'
+      });
+    }
+    req.decoded = decoded;
+    next();
+  });
 
 }
 
@@ -51,6 +64,27 @@ async function run() {
       res.json(result)
     })
 
+
+    app.get("/task", async (req, res) => {
+      const q = req.query;
+      const cursor = taskCollection.find(q);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //Update task
+    app.put('/task/:id',  async (req, res) => {
+      const id = req.params.id;
+      const updatedTask = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: updatedTask,
+      };
+      const result = await taskCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
     //post reviews
     app.post("/customerReviews", async (req, res) => {
       const item = req.body
@@ -58,31 +92,45 @@ async function run() {
       res.json(result)
     })
 
+    //get all Users
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     //Users
     app.put('/user/:email', async (req, res) => {
       const user = req.body;
       const email = req.params.email;
-      const filter = { email: email };
-      const options = { upsert: true };
+      const filter = {
+        email: email
+      };
+      const options = {
+        upsert: true
+      };
       const updateDoc = {
-          $set: user,
+        $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: '1h'
+      const token = jwt.sign({
+        email: email
+      }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
       })
-      res.send({ result, token });
+      res.send({
+        result,
+        token
+      });
 
-  })
-//post task
-  app.post('/task', async (req, res) => {
-    const task = req.body;
-    const result = await taskCollection.insertOne(task);
-    res.send(result);
-})
+    })
+    //post task
+    app.post('/task', async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    })
 
-  }
-  finally {
+  } finally {
 
   }
 }
