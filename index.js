0004d141c63e06ee98cc5e76c9de1b4f08d2076b
Erfath <erfathparvez1@gmail.com.com>
2022-08-17 19:50:04 +0600

    Require Admin & Add Member

--- index.js
+++ index.js
@@ -51,7 +51,19 @@ async function run() {
     const bookingsCollection = database.collection("bookings");
     const paymentsCollection = database.collection('payments');
     const taskCollection = database.collection('tasks');
+    const employeeCollection = database.collection('employee');
 
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester })
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'Forbidden Access' })
+      }
+    }
 
     //get all reviews from database
     app.get("/customerReviews", async (req, res) => {
@@ -68,7 +80,7 @@ async function run() {
     });
 
     //Update task
-    app.put('/task/:id',  async (req, res) => {
+    app.put('/task/:id', async (req, res) => {
       const id = req.params.id;
       const updatedTask = req.body;
       const filter = { _id: ObjectId(id) };
@@ -129,112 +141,119 @@ async function run() {
       const bookings = await bookingsCollection.find().toArray();
       res.send(bookings);
     })
-      //Get Admin
-      app.get('/admin/:email', verifyJWT, async (req, res) => {
-        const email = req.params.email;
-        const user = await userCollection.findOne({ email: email });
-        const isAdmin = user.role === 'Admin';
-        res.send({ admin: isAdmin });
+    //Get Admin
+    app.get('/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'Admin';
+      res.send({ admin: isAdmin });
+    })
+    //Set role
+    app.put('/user_admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const role = req.body;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: role,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    //get tasks
+    app.get("/task", async (req, res) => {
+      const q = req.query;
+      const cursor = taskCollection.find(q);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //post task
+    app.post('/task', async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    })
+
+    //Update task
+    app.put('/task/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedTask = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: updatedTask,
+      };
+      const result = await taskCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    app.get('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking)
+    })
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const filter = { _id: ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(filter);
+      res.send(result);
+    })
+
+    //Payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
       })
-      //Set role
-      app.put('/user_admin/:email', async (req, res) => {
-        const email = req.params.email;
-        const role = req.body;
-        const filter = { email: email };
-        const updateDoc = {
-          $set: role,
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-
-      })
-      //get tasks
-      app.get("/task", async (req, res) => {
-        const q = req.query;
-        const cursor = taskCollection.find(q);
-        const result = await cursor.toArray();
-        res.send(result);
-      });
-
-      //post task
-      app.post('/task', async (req, res) => {
-        const task = req.body;
-        const result = await taskCollection.insertOne(task);
-        res.send(result);
-      })
-
-      //Update task
-      app.put('/task/:id', async (req, res) => {
-        const id = req.params.id;
-        const updatedTask = req.body;
-        const filter = { _id: ObjectId(id) };
-        const updateDoc = {
-          $set: updatedTask,
-        };
-        const result = await taskCollection.updateOne(filter, updateDoc);
-        res.send(result);
-
-      })
-
-      app.get('/bookings/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const booking = await bookingsCollection.findOne(query);
-        res.send(booking)
-      })
-
-      app.delete('/bookings/:id', async (req, res) => {
-        const id = req.params.id;
-        console.log(id)
-        const filter = { _id: ObjectId(id) };
-        const result = await bookingsCollection.deleteOne(filter);
-        res.send(result);
-      })
-
-      //Payment
-      app.post("/create-payment-intent", async (req, res) => {
-        const service = req.body;
-        const price = service.price;
-        const amount = price * 100;
-        const paymentIntent = await stripe.paymentIntents.create({
-          amount: amount,
-          currency: "usd",
-          payment_method_types: ["card"],
-        })
-        res.send({ clientSecret: paymentIntent.client_secret })
-      })
+      res.send({ clientSecret: paymentIntent.client_secret })
+    })
 
 
-      app.patch('/bookings/:id', async (req, res) => {
-        const id = req.params.id;
-        const payment = req.body;
-        const filter = { _id: ObjectId(id) };
-        const updateDoc = {
-          $set: {
-            paid: true,
-            transaction: payment.transaction
-          }
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          paid: true,
+          transaction: payment.transaction
         }
-        const result = await paymentsCollection.insertOne(payment);
-        const updateOrder = await bookingsCollection.updateOne(filter, updateDoc);
-        res.send(updateDoc);
-      })
+      }
+      const result = await paymentsCollection.insertOne(payment);
+      const updateOrder = await bookingsCollection.updateOne(filter, updateDoc);
+      res.send(updateDoc);
+    })
 
-    }
-    finally {}
+
+    app.post('/employee', async (req, res) => {
+      const doctor = req.body;
+      const result = await employeeCollection.insertOne(doctor);
+      res.send(result);
+    })
 
   }
+  finally { }
+
+}
 
 run().catch(console.dir);
 
 
-  app.get('/', (req, res) => {
-    res.send('Welcome to PerformCamp Server!')
-  })
-  app.get('/check', (req, res) => {
-    res.send('Test route checking')
-  })
+app.get('/', (req, res) => {
+  res.send('Welcome to PerformCamp Server!')
+})
+app.get('/check', (req, res) => {
+  res.send('Test route checking')
+})
 
-  app.listen(port, () => {
-    console.log(`PerformCamp Server listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`PerformCamp Server listening on port ${port}`)
+})
